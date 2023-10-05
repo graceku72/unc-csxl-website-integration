219e5b9fd75f23ea32c94693ad3098e7dd8ad1e4
graceku72 <graceku72@gmail.com>
2023-10-05 18:09:56 -0400

    checked if pid length isnt 9

--- frontend/src/app/checkin.service.ts
+++ frontend/src/app/checkin.service.ts
@@ -15,19 +15,21 @@ import { CheckinRequest } from './checkin_request';
 export class CheckinService {
 
   constructor(private http: HttpClient, private registrationService: RegistrationService) { }
-  // getUser(pid: number): User{
-  //   for (let i = 0; i < this.registrationService.getUsers().length; i++){
-  //     if (user.pid == pid){
-  //       return user;
-  //     }
-  //   }
-
-  // }
   getCheckins(): Observable<Checkin[]>{
     return this.http.get<Checkin[]>("/api/checkins");
   }
 
   addCheckin(pid: number): Observable<CheckinRequest> {
+    let errors: string[] = [];
+
+    if (pid.toString().length !== 9) {
+      errors.push(`Invalid PID: ${pid}`);
+    }
+
+    if (errors.length > 0) {
+      return throwError(() => { return new Error(errors.join("\n")) });
+    }
+
     let request: CheckinRequest = {pid}
 
     return this.http.post<CheckinRequest>("/api/checkins", request);
